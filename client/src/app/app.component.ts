11a8a7f66271e6996d0923dc3edc3f96a9ca7c36
Kuba <jciesmail@gmail.com>
2022-05-07 19:17:59 +0200

    add ui

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './models/pagination';
-import { IProduct } from './models/product';
+
 
 @Component({
   selector: 'app-root',
@@ -9,19 +8,12 @@ import { IProduct } from './models/product';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-
   title = 'Skinet';
-	products: IProduct[];
 
-	constructor(private http: HttpClient) {}
+	constructor() {}
 
 	ngOnInit(): void {
-		this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
-			(response: IPagination) => {
-			this.products = response.data;
-		}, error => {
-			console.log(error);
-		});
+	
 	}
 }
 
